df297a844c1d2866473435bfaeb24821c451b0c9
Matheus <matheusyagami147@gmail.com>
2021-10-05 12:00:08 -0300

    definido o express.json() e o express.urlencoded no custom-express.js

--- config/custom-express.js
+++ config/custom-express.js
@@ -4,6 +4,10 @@ const consign = require('consign')
 
 module.exports = function() {
     const app = express()
+
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+
     consign().include('controllers').into(app)
 
     return app
