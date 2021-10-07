b246f0de9d98db7ec36ac64a56a1aaf83f060581
Matheus <matheusyagami147@gmail.com>
2021-10-07 10:14:26 -0300

    desenvolvido o upload controller para upload de arquivos via stream

---
+++ controllers/upload.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+module.exports = function(app){
+
+  app.post("/upload/imagem", function(req, res){
+    console.log('recebendo imagem');
+
+    const filename = req.headers.filename;
+
+    req.pipe(fs.createWriteStream('files/' + filename))
+    .on('finish', function(){
+      console.log('arquivo escrito');
+      res.status(201).send('ok');
+    });
+
+  });
+
+}
