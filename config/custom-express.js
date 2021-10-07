edb6674e75fa20e964be6847aedf76d145645bf4
Matheus <matheusyagami147@gmail.com>
2021-10-07 13:18:23 -0300

    morgan configurado no arquivo de custom-express.js

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const consign = require('consign')
 const expressValidator = require('express-validator')
-
+const morgan = require('morgan')
+const logger = require('../servicos/logger')
 
 
 module.exports = function() {
     const app = express()
+
+    app.use(morgan("common", {
+        stream: {
+          write: function(mensagem){
+              logger.info(mensagem);
+          }
+        }
+    }))    
 
     app.use(express.json())
     app.use(express.urlencoded({extended: true}))
